dbbd1f522c45eee3585008bf0b77690f2ba332dd
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-10-26 17:46:19 -0400

    Initial grammar etc

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct GrammarParser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = std::env::args().skip(1).next().unwrap();
+    let contents = std::fs::read_to_string(&filename)?;
+    let program = GrammarParser::parse(Rule::program, &contents);
+    match program {
+        Ok(_) => {}
+        Err(e) => println!("{}", e),
+    }
+    Ok(())
+}
